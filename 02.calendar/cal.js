c399ae670f7d472dbd1b70eb5f01a213311bab60
ko <kyokucho1989@gmail.com>
2023-10-04 15:12:13 +0900

    lint prettier 実行

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -1,22 +1,22 @@
 #!/usr/bin/env node
 
-import minimist from 'minimist';
+import minimist from "minimist";
 import dayjs from "dayjs";
 
 let argv = minimist(process.argv);
-const month = argv['m'] === undefined ? dayjs().month() + 1 : argv['m'];
-const year = argv['y'] === undefined ? dayjs().year() : argv['y'];
+const month = argv["m"] === undefined ? dayjs().month() + 1 : argv["m"];
+const year = argv["y"] === undefined ? dayjs().year() : argv["y"];
 
 const monthIndex = month - 1;
-let days_to_shift = dayjs(new Date(year,monthIndex,1)).day();
-let day_end = dayjs(new Date(year,monthIndex + 1,0)).date();
+let days_to_shift = dayjs(new Date(year, monthIndex, 1)).day();
+let day_end = dayjs(new Date(year, monthIndex + 1, 0)).date();
 
 console.log(`      ${month}月  ${year}`);
 console.log(`日 月 火 水 木 金 土`);
 process.stdout.write("   ".repeat(days_to_shift));
-for (let day = 1; day <= day_end ; day++ ){
-  process.stdout.write(day.toString().padStart(2, ' ') + " ");
-  if( (day + days_to_shift) % 7 == 0){
+for (let day = 1; day <= day_end; day++) {
+  process.stdout.write(day.toString().padStart(2, " ") + " ");
+  if ((day + days_to_shift) % 7 == 0) {
     console.log("");
   }
 }
