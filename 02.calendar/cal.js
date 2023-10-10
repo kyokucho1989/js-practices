efa4472502aa08ad3a6a408428c8c7bd8e05940f
ko <kyokucho1989@gmail.com>
2023-10-11 02:56:46 +0900

    変数constに変更/ 変数名変更

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -3,20 +3,20 @@
 import minimist from "minimist";
 import dayjs from "dayjs";
 
-let argv = minimist(process.argv);
+const argv = minimist(process.argv);
 const month = argv["m"] === undefined ? dayjs().month() + 1 : argv["m"];
 const year = argv["y"] === undefined ? dayjs().year() : argv["y"];
 
 const monthIndex = month - 1;
-let days_to_shift = dayjs(new Date(year, monthIndex, 1)).day();
-let day_end = dayjs(new Date(year, monthIndex + 1, 0)).date();
+const weekdayShift = dayjs(new Date(year, monthIndex, 1)).day();
+const monthEndDate = dayjs(new Date(year, monthIndex + 1, 0)).date();
 
 console.log(`      ${month}月  ${year}`);
 console.log(`日 月 火 水 木 金 土`);
-process.stdout.write("   ".repeat(days_to_shift));
-for (let day = 1; day <= day_end; day++) {
+process.stdout.write("   ".repeat(weekdayShift));
+for (let day = 1; day <= monthEndDate; day++) {
   process.stdout.write(day.toString().padStart(2, " ") + " ");
-  if ((day + days_to_shift) % 7 == 0) {
+  if ((day + weekdayShift) % 7 == 0) {
     console.log("");
   }
 }
