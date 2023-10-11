bec23a62bde17e9a6223a2aeb53a1a72a98e0eb8
ko <kyokucho1989@gmail.com>
2023-10-12 06:11:57 +0900

    変数名修正

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -4,19 +4,19 @@ import minimist from "minimist";
 import dayjs from "dayjs";
 
 const argv = minimist(process.argv);
-const generatedDate = dayjs();
-const month = argv.m ?? generatedDate.month() + 1;
-const year = argv.y ?? generatedDate.year();
-const weekdayShift = dayjs(new Date(year, month - 1, 1)).day();
+const dateDisplayInCalendar = dayjs();
+const month = argv.m ?? dateDisplayInCalendar.month() + 1;
+const year = argv.y ?? dateDisplayInCalendar.year();
+const monthBeginningDayOfWeek = dayjs(new Date(year, month - 1, 1)).day();
 const monthEndDate = dayjs(new Date(year, month, 0)).date();
 
 console.log(`      ${month}月  ${year}`);
 console.log("日 月 火 水 木 金 土");
-process.stdout.write("   ".repeat(weekdayShift));
-for (let day = 1; day <= monthEndDate; day++) {
-  process.stdout.write(day.toString().padStart(2, " ") + " ");
-  if ((day + weekdayShift) % 7 === 0) {
+process.stdout.write("   ".repeat(monthBeginningDayOfWeek));
+for (let date = 1; date <= monthEndDate; date++) {
+  process.stdout.write(date.toString().padStart(2, " ") + " ");
+  if ((date + monthBeginningDayOfWeek) % 7 === 0) {
     console.log();
   }
 }
-if (weekdayShift !== 5) console.log();
+if (monthBeginningDayOfWeek !== 5) console.log();
