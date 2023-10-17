4138c98b05487ea2e2798202908e9d94317f80dd
ko <kyokucho1989@gmail.com>
2023-10-18 04:26:12 +0900

    dbをひとつだけに変更/ エラーなしとありの間にタイマー追加

--- 03.asynchronous/callback.js
+++ 03.asynchronous/callback.js
@@ -1,11 +1,11 @@
 #!/usr/bin/env node
 
 import sqlite3 from "sqlite3";
+import timers from "timers/promises";
 
 const db = new sqlite3.Database(":memory:");
-const db2 = new sqlite3.Database(":memory:");
 
-// コールバック
+// コールバック エラーなし
 db.run("CREATE TABLE lorem (title TEXT NOT NULL)", [], () => {
   db.run("INSERT INTO lorem (title) VALUES ('本のタイトル') ", [], function () {
     console.log("挿入された行のID:", this.lastID);
@@ -19,38 +19,36 @@ db.run("CREATE TABLE lorem (title TEXT NOT NULL)", [], () => {
   });
 });
 
-db.close(f2);
+await timers.setTimeout(1000);
 
 // コールバック エラーあり
-function f2() {
-  db2.run("CREATE TABLE lorem2 (title TEXT NOT NULL)", [], () => {
-    db2.run(
-      "INSERT INTO lorem2 (title2) VALUES ('本のタイトル2') ",
-      [],
-      function (err) {
-        let id;
-        if (err) {
-          console.error(err.message);
-        } else {
-          console.log("挿入された行のID:", this.lastID);
-          id = this.lastID;
-        }
-        db2.get(
-          `SELECT * FROM lorem2 WHERE rowid = ?`,
-          [id],
-          function (err, rows) {
-            if (err) {
-              console.error(err.message);
-            } else if (rows === undefined) {
-              console.log("レコードが見つかりません");
-            }
-            db2.run("DROP TABLE lorem2", [], function () {
-              console.log("テーブル削除完了");
-            });
-          }
-        );
+db.run("CREATE TABLE lorem2 (title TEXT NOT NULL)", [], () => {
+  db.run(
+    "INSERT INTO lorem2 (title2) VALUES ('本のタイトル2') ",
+    [],
+    function (err) {
+      let id;
+      if (err) {
+        console.error(err.message);
+      } else {
+        console.log("挿入された行のID:", this.lastID);
+        id = this.lastID;
       }
-    );
-  });
-  db2.close();
-}
+      db.get(
+        `SELECT * FROM lorem2 WHERE rowid = ?`,
+        [id],
+        function (err, rows) {
+          if (err) {
+            console.error(err.message);
+          } else if (rows === undefined) {
+            console.log("レコードが見つかりません");
+          }
+          db.run("DROP TABLE lorem2", [], function () {
+            console.log("テーブル削除完了");
+          });
+        }
+      );
+    }
+  );
+  db.close();
+});
