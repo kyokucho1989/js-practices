8edc4fc711c34306267af8c186cdb5baf4ea77e5
ko <kyokucho1989@gmail.com>
2023-10-20 04:10:44 +0900

    テンプレートリテラルの使い分けを修正

--- 03.asynchronous/callback.js
+++ 03.asynchronous/callback.js
@@ -8,8 +8,8 @@ const db = new sqlite3.Database(":memory:");
 // コールバック エラーなし
 db.run("CREATE TABLE books (title TEXT NOT NULL)", () => {
   db.run("INSERT INTO books (title) VALUES ('本のタイトル') ", function () {
-    console.log("挿入された行のID:" + this.lastID);
-    db.get(`SELECT * FROM books WHERE rowid = ?`, this.lastID, (_err, book) => {
+    console.log("挿入された行のID:", this.lastID);
+    db.get("SELECT * FROM books WHERE rowid = ?", this.lastID, (_err, book) => {
       console.log(book);
       db.run("DROP TABLE books", () => {
         console.log("テーブル削除完了");
@@ -26,9 +26,9 @@ db.run("CREATE TABLE books (title TEXT NOT NULL)", () => {
     if (err) {
       console.error(err.message);
     } else {
-      console.log("挿入された行のID:" + this.lastID);
+      console.log("挿入された行のID:", this.lastID);
     }
-    db.get(`SELECT * FROM bookss WHERE rowid = ?`, this.lastID, (err, book) => {
+    db.get("SELECT * FROM bookss WHERE rowid = ?", this.lastID, (err, book) => {
       if (err) {
         console.error(err.message);
       } else {
