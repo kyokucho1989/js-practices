c0467dc80a179753645dbf5ab05c7bf7e195c58e
ko <kyokucho1989@gmail.com>
2023-10-26 05:59:34 +0900

    top level awaitに対応

--- 03.asynchronous/async-await.js
+++ 03.asynchronous/async-await.js
@@ -7,56 +7,49 @@ import { get } from "./sqlite-function.js";
 
 const db = new sqlite3.Database(":memory:");
 
-async function promiseOk() {
-  await run(db, "CREATE TABLE books (title TEXT NOT NULL)");
-  const insertedBook = await run(db, "INSERT INTO books (title) VALUES (?) ", [
-    "本のタイトル"
-  ]);
-  console.log(`挿入された行のID: ${insertedBook.lastID}`);
-  const selectedBook = await get(db, "SELECT * FROM books WHERE rowid = ?", [
-    insertedBook.lastID
-  ]);
-  console.log(selectedBook);
-  await run(db, "DROP TABLE books");
-  console.log("テーブル削除完了");
-}
+// エラーなし
 
-async function promiseError() {
-  let insertedBook, selectedBook;
+await run(db, "CREATE TABLE books (title TEXT NOT NULL)");
+let insertedBook = await run(db, "INSERT INTO books (title) VALUES (?) ", [
+  "本のタイトル"
+]);
+console.log(`挿入された行のID: ${insertedBook.lastID}`);
+let selectedBook = await get(db, "SELECT * FROM books WHERE rowid = ?", [
+  insertedBook.lastID
+]);
+console.log(selectedBook);
+await run(db, "DROP TABLE books");
+console.log("テーブル削除完了");
+
+await timers.setTimeout(1000);
+
+// エラーあり
+
+try {
+  await run(db, "CREATE TABLE books (title TEXT NOT NULL)");
   try {
-    await run(db, "CREATE TABLE books (title TEXT NOT NULL)");
-    try {
-      insertedBook = await run(
-        db,
-        "INSERT INTO books (title) VALUES (?) ",
-        null
-      );
-      console.log(`挿入された行のID: ${insertedBook.lastID}`);
-      selectedBook = await get(db, "SELECT * FROM books WHERE rowid = ?", [
-        insertedBook.lastID
-      ]);
-    } catch (err) {
-      console.error(err.message);
-      selectedBook = await get(db, "SELECT * FROM bookss WHERE rowid = ?", [1]);
-    }
-    try {
-      console.log(insertedBook);
-      selectedBook = await get(db, "SELECT * FROM books WHERE rowid = ?", [
-        insertedBook.lastID
-      ]);
-      console.log(selectedBook);
-    } catch (err) {
-      console.error(err.message);
-    }
+    insertedBook = await run(db, "INSERT INTO books (title) VALUES (?) ", null);
+    console.log(`挿入された行のID: ${insertedBook.lastID}`);
+    selectedBook = await get(db, "SELECT * FROM books WHERE rowid = ?", [
+      insertedBook.lastID
+    ]);
   } catch (err) {
     console.error(err.message);
-  } finally {
-    await run(db, "DROP TABLE books");
-    console.log("テーブル削除完了");
-    db.close();
+    selectedBook = await get(db, "SELECT * FROM bookss WHERE rowid = ?", [1]);
   }
+  try {
+    console.log(insertedBook);
+    selectedBook = await get(db, "SELECT * FROM books WHERE rowid = ?", [
+      insertedBook.lastID
+    ]);
+    console.log(selectedBook);
+  } catch (err) {
+    console.error(err.message);
+  }
+} catch (err) {
+  console.error(err.message);
+} finally {
+  await run(db, "DROP TABLE books");
+  console.log("テーブル削除完了");
+  db.close();
 }
-
-promiseOk();
-await timers.setTimeout(1000);
-promiseError();
